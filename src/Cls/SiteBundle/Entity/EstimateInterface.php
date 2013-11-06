d7eca6d74b1facdb94347a85c98b581486ea57c5
Eduardo Borges <eduardomrb@gmail.com>
2013-11-05 21:10:24 -0300

    a implementar interface de orçamento

---
+++ src/Cls/SiteBundle/Entity/EstimateInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cls\SiteBundle\Entity;
+
+interface EstimateInterface
+{
+    
+}
---
+++ src/Cls/SiteBundle/Mail/EstimateNotification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cls\SiteBundle\Mail;
+
+use Cls\SiteBundle\Entity\EstimateInterface;
+
+class EstimateNotification
+{
+    public function sendMailNotification(EstimateInterface $estimate)
+    {
+        
+    }
+}
